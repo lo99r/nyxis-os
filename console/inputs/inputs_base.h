a3e3b153103fbde3b9e40a76550f05ff71f6bce3
tamier <perpectfieldvision@gmail.com>
2026-04-24 22:47:19 +0900

    Refactor types and includes in inputs_base.h

--- console/inputs/inputs_base.h
+++ console/inputs/inputs_base.h
@@ -1,33 +1,32 @@
 #ifndef INPUTS_BASE_H
 #define INPUTS_BASE_H
 
-#include <Uefi.h>
-#include <stdint.h>
+#include "nyxis.h"
 
 // ==========================
 // 포트 I/O
 // ==========================
-static inline uint8_t inb(uint16_t port);
+static inline u8 inb(uint16_t port);
 static inline void io_wait(void);
 
 // ==========================
 // 키보드 상태 (전역 변수)
 // ==========================
-extern UINT8 shift_l;
-extern UINT8 shift_r;
-extern UINT8 caps_lock;
-extern UINT8 extended;
+extern u8 shift_l;
+extern u8 shift_r;
+extern u8 caps_lock;
+extern u8 extended;
 
 // ==========================
 // ASCII 테이블
 // ==========================
-extern char normal[128];
-extern char shifted[128];
+extern utf8 normal[128];
+extern utf8 shifted[128];
 
 // ==========================
 // 스캔코드 / 입력 함수
 // ==========================
-uint8_t get_scancode(void);
+u8 get_scancode(void);
 char translate(uint8_t sc);
 char get_char(void);
 
