a20ecdde8aa602c0aa45e79f7a5c4a9db5ecfd11
plertube2bd <perpectfieldvision@gmail.com>
2026-04-27 15:55:21 +0900

    Hello

--- types.h
+++ types.h
@@ -1,6 +1,8 @@
 #ifndef NYXIS_TYPES_H
 #define NYXIS_TYPES_H
 
+#include <stdint.h>
+
 // fixed size
 typedef unsigned char      u8;
 typedef unsigned short     u16;
@@ -12,11 +14,14 @@ typedef short     i16;
 typedef int       i32;
 typedef long long i64;
 
+
 // pointer-sized
 #if UINTPTR_MAX == 0xFFFFFFFFFFFFFFFF
+    #define __nyxis_64bits
     typedef u64 usize;
     typedef i64 isize;
 #else
+    #define __nyxis_32bits
     typedef u32 usize;
     typedef i32 isize;
 #endif
@@ -24,5 +29,43 @@ typedef long long i64;
 // chars
 typedef u8  utf8;
 typedef u16 utf16;
+
+
+// =====================
+// error system
+// =====================
+
+// status type
+typedef i32 Nstatus;
+
+// MSB = error flag
+#define NSTATUS_ERR_FLAG ((i32)1 << 31)
+
+// helpers
+#define NSTATUS_OK 0
+#define NSTATUS_MAKE_ERR(code) (NSTATUS_ERR_FLAG | (code))
+#define NSTATUS_IS_ERR(x) ((x) < 0)
+#define NSTATUS_CODE(x) ((x) & 0x7FFFFFFF)
+
+
+// error codes (16개)
+typedef enum {
+    NnotFound        = NSTATUS_MAKE_ERR(1),
+    NinvalidArg      = NSTATUS_MAKE_ERR(2),
+    NoutOfMemory     = NSTATUS_MAKE_ERR(3),
+    Npermission      = NSTATUS_MAKE_ERR(4),
+    Ntimeout         = NSTATUS_MAKE_ERR(5),
+    Nbusy            = NSTATUS_MAKE_ERR(6),
+    NalreadyExists   = NSTATUS_MAKE_ERR(7),
+    Noverflow        = NSTATUS_MAKE_ERR(8),
+    Nunderflow       = NSTATUS_MAKE_ERR(9),
+    Nio              = NSTATUS_MAKE_ERR(10),
+    Ndisconnected    = NSTATUS_MAKE_ERR(11),
+    Nunsupported     = NSTATUS_MAKE_ERR(12),
+    Ncorrupted       = NSTATUS_MAKE_ERR(13),
+    Ninterrupted     = NSTATUS_MAKE_ERR(14),
+    Nunknown         = NSTATUS_MAKE_ERR(15),
+    Nfatal           = NSTATUS_MAKE_ERR(16)
+} Nerror;
 
 #endif
