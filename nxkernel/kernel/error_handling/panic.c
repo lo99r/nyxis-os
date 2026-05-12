0a77772f1778dd0781bd835a4367661f8e478a64
tamier <perpectfieldvision@gmail.com>
2026-05-12 19:04:33 +0900

    Enhance kernel panic message formatting

--- nxkernel/kernel/error_handling/panic.c
+++ nxkernel/kernel/error_handling/panic.c
@@ -2,19 +2,21 @@
 
 #include "include/nyxis.h"
 #include "include/lowlevel.h"
+#include "kernel/paging.h"
 
 __attribute__((noreturn))
 static inline void kernel_panic_simple(const char* Message, Nstatus error) {
+    disable_paging();
     printk("\n");
     printk("========================================\n");
     printk("            KERNEL PANIC\n");
     printk("========================================\n");
 
     if (Message) {
-        printk("Reason: %s%r\n", Message, error);
+        printk("Reason: %s code: %ux(%r)\n", Message, error, error);
     }
 
-    printk("System halted.\n");
+    printk("System halted. should reboot.\n");
 
     for (;;) {
         cli();
